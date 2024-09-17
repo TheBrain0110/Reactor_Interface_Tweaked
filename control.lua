06b3afeacebb9d90745fed6c599a6c979787e0aa
robot256 <robot256@GMAIL.COM>
2024-09-17 18:56:39 -0400

    Fix crash when reactor is deregistered
    Fix crash if a reactor is deregistered in the middle of an update cycle just before its turn to be serviced.  This resets the update cycle index so the iteration is not corrupted. (Not sure if this will repeat the update cycle immediately, or if some reactors won't be updated until the next cycle.)

--- control.lua
+++ control.lua
@@ -95,6 +95,9 @@ function deregister_interface(reactor)
     global.reactors[reactor.unit_number].interface.destroy()
     global.reactors[reactor.unit_number] = nil
     reactor.active = true
+    if global.reactors_index == reactor.unit_number then
+      global.reactors_index = nil
+    end
   end
 end
 
